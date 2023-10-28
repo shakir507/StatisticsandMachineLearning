7f84643fed0c2c8e69f6dc6a526a5fce230f21a8
shakir507 <shakir.bilal@gmail.com>
2023-10-28 16:31:08 -0400

    Working Dirichlet likelihood

--- MachineLearning/MLEwithDirichlete.R
+++ MachineLearning/MLEwithDirichlete.R
@@ -2,14 +2,16 @@
 
 
 #data section begins
-n<-199*2
-x<-c()
+#observed data is the dengue serotype proportions,
+#where only three serotypes are assumes in a year for example
+observed_data<-c(0.1,0.5,0.4)
 #data section ends
-
+alpha1<-c(0,0,0)
 #the likelihood function for an observed data with three variables, x1,x2,x3 is given by
-
-DirLik<-function(p,x,n){
-  lk<-p[1]^x[1]*p[2]^x[2]*p[3]^x[3]
+alpha<-c(2,3,4)
+DirLik<-function(alpha,observed_data){
+  nrm<-gamma(sum(alpha))
+  lk<-prod(observed_data^alpha /gamma(alpha))/nrm
   return (lk)
 }
 
@@ -17,19 +19,28 @@ DirLik<-function(p,x,n){
 
 # we can in the meanwhile plot and check the parameter value where the function might achieve its maximum:
 dp<-0.01
-lk<-array(0,integer(1/dp))
-i=1
+lk<-matrix(0,nrow=5,ncol=5)
+i1<-1
 max1<- -10
-pmax<-0
-for(p in seq(0,1,0.01)){
-    p=c(p1,p2,p3)
+pmax<-c(0,0,0)
+for(p1 in seq(1,5,1)){
+    j1<-1
+    for(p2 in seq(1,5,1)){
+        p3=11-p1-p2
+    alpha=c(p1,p2,p3)
 
-  lk[i]<-DirLik(p,x1,n)
-  if(lk[i]>max1){
-    max1<-lk[i]
-    pmax<-p
+  lk[i1,j1]<-DirLik(alpha,observed_data)
+  if(lk[i1,j1]>max1){
+    max1<-lk[i1,j1]
+    alpha1<-alpha
   }
-  i=i+1
+  print(c(i1,j1,lk[i1,j1]))
+
+  j1=j1+1
+    }
+  i1=i1+1
 }
-print(c(pmax,max1))
-plot(seq(0,1,0.01),lk,type='b',col='red')
+print(lk)
+print("The most likely parameters that will produce the given data from a dirichlter distribution are")
+print(c(alpha1,max1))
+# plot(seq(0,1,0.01),lk,type='b',col='red')
