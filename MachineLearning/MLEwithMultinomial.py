1fc75f3cec31d7cf2c78fcbff3eea78ea7a6a328
shakir507 <shakir.bilal@gmail.com>
2023-10-30 21:53:06 -0400

    Multinomial update

--- MachineLearning/MLEwithMultinomial.py
+++ MachineLearning/MLEwithMultinomial.py
@@ -1,32 +1,44 @@
 import numpy as np
 from scipy.stats import multinomial
 
-# Observed data (example data for three categories)
-observed_data = [1, 4, 7]
-max1=-1000
-lk=np.zeros((10,10))
-# Probabilities for each category
-i1=0
-im=0
-jm=0
-for p1 in np.arange(0,1,0.1):
-    j1=0
-    for p2 in np.arange(0,1,0.1):
-        p3=1-p1-p2
-        if(p3>0):
-            probabilities = [p1, p2, p3]
-            # Calculate the likelihood using the Multinomial distribution
-            likelihood = multinomial.pmf(observed_data, n=np.sum(observed_data), p=probabilities)
-            lk[i1][j1]=likelihood
-            if(likelihood>max1):
-                max1=likelihood
-                im=p1
-                jm=p2
-            formatted_value = "{:.3f}".format(lk[i1][j1])
-            print(formatted_value,end=" ")
-        j1+=1
-    print("\n")
-    i1+=1
 
-print("Maximum Likelihood:",likelihood, max1,im,jm,1-im-jm)
-# print(lk)
+
+def MultinomDist(p,observed_data):
+    lk=multinomial.pmf(observed_data, n=np.sum(observed_data), p=p)
+    return lk
+
+def likelihood(observed_data):
+    npoints=10
+    lk=np.zeros((npoints,npoints))
+    # Probabilities for each category
+    i1=0
+    for p1 in np.arange(0,1,1.0/npoints):
+        j1=0
+        for p2 in np.arange(0,1,1.0/npoints):
+            p3=1-p1-p2
+            if(p3>0):
+                probabilities = [p1, p2, p3]
+                # Calculate the likelihood using the Multinomial distribution
+                likelihood = multinomial.pmf(observed_data, n=np.sum(observed_data), p=probabilities)
+                lk[i1][j1]=likelihood
+                # formatted_value = "{:.3f}".format(lk[i1][j1])
+                # print(formatted_value,end=" ")
+            j1+=1
+        i1+=1
+    return lk
+
+if __name__=="__main__":
+    #data section begins
+    # Observed data (example data for three categories)
+    observed_data = [1, 4, 7]
+                
+    lk=likelihood(observed_data)
+    # Find the index of the maximum value in the entire matrix
+    max_index = np.argmax(lk)
+
+    # Convert the flattened index to row and column indices
+    row_index, col_index = np.unravel_index(max_index, lk.shape)
+    max1=max(lk.flatten())
+    print("Maximum Likelihood:", max1,row_index,col_index)
+    # formatted_value = "{:.3f}".format(lk)
+    # print(formatted_value,end=" ")
